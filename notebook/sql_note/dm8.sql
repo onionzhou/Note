a02a2ec29065b00ee147b13b312bea80485a2a87
onion <892723057@qq.com>
2021-12-02 16:51:44 +0800

    auto bak

--- notebook/sql_note/dm8.sql
+++ notebook/sql_note/dm8.sql
@@ -25,3 +25,27 @@ create table "student"
 INSERT INTO "student"  VALUES(123,'qwde');
 
 select * from "student";
+
+
+
+达梦7
+1.查询会话中程序占用会话数
+
+select appname,count(*) from v$sessions group by  "V$SESSIONS".APPNAME;
+2.查看用户下指定表占用空间
+
+SELECT TABLE_USED_SPACE('username','tablename')*SF_GET_PAGE_SIZE()/1024/1024||'M'
+3.查看用户下指定表实际使用的空间
+
+SELECT TABLE_USED_PAGES('unsername','tablename')*SF_GET_PAGE_SIZE()/1024/1024||'M'
+
+达梦6
+1.查询cpu占用较长时间未释放的sql
+
+select sql_text,app_name,login_name,cpu_time_call frome v$session where cpu_time_call > 1000;
+
+
+
+
+
+
---
+++ notebook/sql_note/kingbase.sql
@@ -0,0 +1,11 @@
+1. 查询数据库名
+
+# select * from SYS_DATABASE;
+1
+2. 查询模式名
+
+# select * from SYS_NAMESPACE;
+1
+3. 查询表空间
+
+# select * from SYS_TABLESPACE;
